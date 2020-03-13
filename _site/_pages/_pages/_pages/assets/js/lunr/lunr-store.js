aba0c1905417c93d5c0320a11d2258fe1ccfa18c
shadow3g <shadow3g@gmail.com>
2020-03-12 18:32:51 -0700

    update github

---
+++ _site/_pages/_pages/_pages/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Data Wrangling Project",
+        "excerpt":"H1 Heading H2 Heading H3 Heading Here’s some basic text. And here’s some italics Here’s some bold text. What about a link? Here’s a bulleted list: First item Second item Third item Here’s a numbered list: First Second Third Python code block: import numpy as np def test_function(x, y): z...","categories": [],
+        "tags": ["data wrangling","data science","messy data"],
+        "url": "http://192.168.9.220:4000/perceptron/",
+        "teaser": null
+      }]
